4d7cb0f6cd09f8c65262b2e7e0b4b96d0a724041
Ådne-Sigurd Leirdal <asleirdal@uia.no>
2026-02-23 16:05:58 +0100

    Base commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Camera_DECT;
+using Radzen;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddRadzenComponents();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
